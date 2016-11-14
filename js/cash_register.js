10a405c50cf48cfdaf1a9e0acb954ee162171f38
Darcey Iwashita <darcey@gmail.com>
2016-11-13 17:32:23 -1000

    added eventlisteners

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,4 +1,4 @@
-console.log("cashregister works!");
+console.log("cash register works now!");
 
 function cashReg(){
   var _balance = 0; //amount in memory 
@@ -21,19 +21,90 @@ function cashReg(){
     return _balance;
   }
  return {
-  balance : _balance,
-  withdraw: _withdraw,
-  deposit: _deposit,
+  balance : balance,
+  withdraw: withdraw,
+  deposit: deposit,
  };
 }
 var register = cashReg();
-console.log(register.balance());
-console.log(register.withdraw());
-console.log(register.deposit());
+// console.log(register.balance());
+// console.log(register.withdraw());
+// console.log(register.deposit());
 
 // CALC BUTTON VALUES
 
-var display = document.getElementById('display');
-display.innerHTML = "some text";
+var display = document.getElementById("display");
 
-// console.log(display);some
+// DECLARING CALCULATOR VARIABLES
+var one = document.getElementById("one");
+var two = document.getElementById("two");
+var three = document.getElementById("three");
+var four = document.getElementById("four");
+var five = document.getElementById("five");
+var six = document.getElementById("six");
+var seven = document.getElementById("seven");
+var eight = document.getElementById("eight");
+var nine = document.getElementById("nine");
+var zero = document.getElementById("zero");
+var doubleZero = document.getElementById("doubleZero");
+var decimal = document.getElementById("decimal");
+var clearButton = document.getElementById("clearButton");
+
+
+// CALCULATOR EVENT LISTENERS
+one.addEventListener("click", function() {
+  display.innerHTML = "1";
+});
+
+two.addEventListener("click", function() {
+  display.innerHTML = "2";
+});
+
+three.addEventListener("click", function() {
+  display.innerHTML = "3";
+});
+
+four.addEventListener("click", function() {
+  display.innerHTML = "4";
+});
+
+five.addEventListener("click", function() {
+  display.innerHTML = "5";
+});
+
+six.addEventListener("click", function() {
+  display.innerHTML = "6";
+});
+
+seven.addEventListener("click", function() {
+  display.innerHTML = "7";
+});
+
+eight.addEventListener("click", function() { 
+  display.innerHTML = "8";
+});
+
+nine.addEventListener("click", function() {
+  display.innerHTML = "9";
+});
+
+zero.addEventListener("click", function() {
+  display.innerHTML = "0";
+});
+
+doubleZero.addEventListener("click", function() {
+  display.innerHTML = "00";
+});
+
+decimal.addEventListener("click", function() {
+  display.innerHTML = ".";
+});
+
+clearButton.addEventListener("click", function() {
+  display.innerHTML = "0";
+});
+
+// function myFunction(button) {
+//   var x = button.value;
+//   document.getElementById("two").innerHTML = x;
+// }
