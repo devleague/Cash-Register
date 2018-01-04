78734f8c0cd5796b855523f5492a3c3d4429b6e8
quinlayen <quinlayen@gmail.com>
2018-01-03 22:29:44 -1000

    number buttons activating

---
+++ calculator.js
@@ -0,0 +1,30 @@
+//window.onload = function (){
+
+/*
+
+var calculator = (function () {
+
+    let _total = '';
+    let _saved_total = '';
+
+
+    function load(x){
+        _total = x;
+        return _total;
+    }
+
+    function add(x){
+        _total += x;
+    }
+
+    function subtract(x){
+        _total -= x;
+    }
+
+    function saveTotal(){
+        _saved_total += _total;
+    }
+
+})();
+*/
+//}
